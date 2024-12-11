19f94149d335630fae0255587367ab9ed87c74fd
joelsiby02 <joelag1235@gmail.com>
2024-12-11 19:19:55 +0530

    Form Done for Login & SignUp - Working fine

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 # login route 
-@auth.route('/login')
+# @auth.route('/login', methods = ['GET', 'POST'])
+# def login():
+#     data = request.form
+#     print(data)
+#     return render_template("login.html")
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print(f'Email: {email}, Password: {password}')
     return render_template("login.html")
 
 # Signup route
 @auth.route('/sign-up')
 def sign_up():
-    return render_template("sign_up.html")
+    return render_template("sign_up.html",  methods = ['GET', 'POST'])
 
 # logout route
 # @auth.route('/logout')
